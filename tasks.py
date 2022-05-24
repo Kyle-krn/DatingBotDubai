cdcf09b41aba846bc78f8375a792fa903e211da6
Egor <egorjkee96@gmail.com>
2022-05-24 15:35:36 +0300

    Fix spam

--- tasks.py
+++ tasks.py
@@ -6,6 +6,7 @@ from keyboards.inline.inline_keyboards import like_keyboard, one_button_keyboard
 from models import models
 from aiogram import Bot
 from tortoise.queryset import Q
+from models.db_query import calculation_users
 # from models.db_query import rowsql_likes
 from utils.text_for_ad import generate_ad_text
 from aiogram.utils.exceptions import BotBlocked
@@ -18,12 +19,14 @@ async def update_likes():
 
 async def spam_motivation_message(bot: Bot):
     '''Раз в час делает рассылку'''
-    list_users = await models.UserModel.filter(Q(ban=False) & Q(tmz__isnull=False))
+    list_users = await models.UserModel.filter(Q(ban=False) & Q(place_id__isnull=False))
+    print(list_users)
     for user in list_users:
         if user.spam_ad_ids is None:
             user.spam_ad_ids = []
             await user.save()
-        local_time_user = datetime.utcnow() + timedelta(hours=user.tmz)
+        
+        local_time_user = datetime.utcnow() + timedelta(hours=(await user.place).tmz)
         if datetime(day=local_time_user.day,            # Если у юзера день 
                     month=local_time_user.month, 
                     year=local_time_user.year,
@@ -46,63 +49,83 @@ async def spam_motivation_message(bot: Bot):
                        "Пожалуйста установите фотографию профиля по кнопке добавить фото"
                     return await bot.send_message(chat_id=user.tg_id, text=text, reply_markup=await one_button_keyboard(text="Добавить фото", callback="change_ava:"))
             elif user.verification is True:
-                your_likes_view = await rowsql_likes(user_id=user.id)
+                # your_likes_view = await rowsql_likes(user_id=user.id)
+                users_like_you = await calculation_users(user_id=user.id, like_catalog=True)
                 likes_view = None
                 user_view = None
-                if len(your_likes_view) > 0:
-                    your_likes_id = [i['id'] for i in your_likes_view]
-                    while True:
+                if len(users_like_you) > 0:
+                    # users_id_like_you = [i['target_id'] for i in users_like_you]
+                    while len(users_like_you) > 0:
                         '''Ищем просмотры где лайкнули юзера'''
-                        if len(your_likes_id) == 0:
-                            break
-                        view_id = your_likes_id.pop(0)
-                        if view_id not in user.spam_ad_ids:
-                            likes_view = await models.UserView.get(id=view_id)
+                        target_user_row= users_like_you.pop(0)
+                        if target_user_row['target_id'] not in user.spam_ad_ids:
+                            target_user = await models.UserModel.get(id=target_user_row['target_id'])
+                            likes_view = await models.UserView.get_or_create(user=user, target_user=target_user)
+                            likes_view = likes_view[0]
                             user.spam_ad_ids.append(likes_view.id)
                             await user.save()
-                            break
-                query = Q(relation__percent_compatibility__gt=0) & Q(target_user__verification=True) & Q(target_user__ban=False) & Q(like=False) & Q(superlike=False)
-                user_views_list = await user.user_view.filter(query).order_by('dislike', 'count_view', '-relation__percent_compatibility')
-                if len(user_views_list) > 0:
-                    while True:
+                            return await send_motivation(user=user, user_view=likes_view, likes=True, general_percent=target_user_row['general_percent'], bot=bot)
+                            # break
+                # query = Q(relation__percent_compatibility__gt=0) & Q(target_user__verification=True) & Q(target_user__ban=False) & Q(like=False) & Q(superlike=False)
+                # user_views_list = await user.user_view.filter(query).order_by('dislike', 'count_view', '-relation__percent_compatibility')
+                users_ralation = await calculation_users(user_id=user.id)
+                if len(users_ralation ) > 0:
+                    while len(users_ralation)  > 0:
                         '''Ищем просмотры юзера'''
-                        if len(user_views_list) == 0:
-                            break
-                        user_view_row = user_views_list.pop(0)
-                        if user_view_row.id not in user.spam_ad_ids:
-                            user_view = user_view_row
-                            user.spam_ad_ids.append(user_view_row.id)
+                        target_user_row = users_ralation.pop(0)
+                        if target_user_row['target_id'] not in user.spam_ad_ids:
+                            target_user = await models.UserModel.get(id=target_user_row['target_id'])
+                            user_view = await models.UserView.get_or_create(user=user, target_user=target_user)
+                            user_view = user_view[0]
+                            user.spam_ad_ids.append(target_user_row['target_id'])
                             await user.save()
-                            break
-                if likes_view is not None:
-                    user_view = likes_view
-                    text = "<b>Тебя лайкнули! Если тебе нравится этот профиль, поставь лайк в ответ и получи контакт для общения!</b>\n\n"
-                elif user_view is not None:
-                    text = "<b>Тебе нравится этот профиль? Что если это взаимно? Поставь лайк и возможно это принесет тебе новое знакомство!</b>\n\n"
-                else:
-                    continue
+                            return await send_motivation(user=user, user_view=user_view, likes=False, general_percent=target_user_row['general_percent'], bot=bot)
+                            
+                        
 
-                user_view.count_view += 1
-                await user_view.save()
-                target_user = await user_view.target_user
-                avatar = await target_user.avatar
-                    
-                text += await generate_ad_text(target_user=target_user, relation=await user_view.relation)
-                if avatar.file_type.lower() in PHOTO_TYPES:
-                    try:
-                        await bot.send_photo(chat_id=user.tg_id, photo=avatar.file_id, caption=text, reply_markup=await like_keyboard(view_id=user_view.id, superlike_count=user.superlike_count)) 
-                    except BotBlocked:
-                        pass
-                elif avatar.file_type.lower() in VIDEO_TYPES:
-                    try:
-                        await bot.send_video(chat_id=user.tg_id, video=avatar.file_id, caption=text, reply_markup=await like_keyboard(view_id=user_view.id, superlike_count=user.superlike_count))
-                    except BotBlocked:
-                        pass
 
+                # if likes_view is not None:
+                #     user_view = likes_view
+                #     text = "<b>Тебя лайкнули! Если тебе нравится этот профиль, поставь лайк в ответ и получи контакт для общения!</b>\n\n"
+                # elif user_view is not None:
+                #     text = "<b>Тебе нравится этот профиль? Что если это взаимно? Поставь лайк и возможно это принесет тебе новое знакомство!</b>\n\n"
+                # else:
+                #     continue
+
+                
+
+async def send_motivation(user: models.UserModel, 
+                          user_view: models.UserView, 
+                          general_percent: int, 
+                          likes: bool, bot):
+    text = "<b>Тебя лайкнули! Если тебе нравится этот профиль, поставь лайк в ответ и получи контакт для общения!</b>\n\n" if likes is True \
+           else "<b>Тебе нравится этот профиль? Что если это взаимно? Поставь лайк и возможно это принесет тебе новое знакомство!</b>\n\n"
+    user_view.count_view += 1
+    await user_view.save()
+    target_user = await user_view.target_user
+    avatar = await target_user.avatar
+        
+    text += await generate_ad_text(target_user=target_user, general_percent=general_percent)
+    if avatar.file_type.lower() in PHOTO_TYPES:
+        try:
+            await bot.send_photo(chat_id=user.tg_id, photo=avatar.file_id, caption=text, reply_markup=await like_keyboard(view_id=user_view.id, 
+                                                                                                                            superlike_count=user.superlike_count,
+                                                                                                                            general_percent=general_percent,
+                                                                                                                            callback="single_reaction")) 
+        except BotBlocked:                                                                      
+            pass
+    elif avatar.file_type.lower() in VIDEO_TYPES:
+        try:
+            await bot.send_video(chat_id=user.tg_id, video=avatar.file_id, caption=text, reply_markup=await like_keyboard(view_id=user_view.id, 
+                                                                                                                          superlike_count=user.superlike_count,
+                                                                                                                           general_percent=general_percent,
+                                                                                                                           callback="single_reaction"))
+        except BotBlocked:
+                        pass
 
 async def scheduler(bot):
     aioschedule.every(1).hours.do(spam_motivation_message, bot)
-    aioschedule.every().day.at("12:00").do(update_likes)
+    # aioschedule.every().day.at("12:00").do(update_likes)
     while True:
         await aioschedule.run_pending() 
         await asyncio.sleep(1)
