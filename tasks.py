f3992e1cf530a12f7d09d681f068b0d3f93e67af
Egor <egorjkee96@gmail.com>
2022-05-30 22:43:48 +0300

    fix

--- tasks.py
+++ tasks.py
@@ -114,7 +114,7 @@ async def send_motivation(user: models.UserModel,
             pass
 
 async def scheduler(bot):
-    aioschedule.every(10).seconds.do(spam_motivation_message, bot)
+    aioschedule.every(1).hours.do(spam_motivation_message, bot)
     aioschedule.every().day.at("12:00").do(update_likes)
     while True:
         await aioschedule.run_pending() 
